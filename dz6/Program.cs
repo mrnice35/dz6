0bd6c76d4de040d429b869340a648ce04970bf9e
gavrilenkovi@bk.ru <gavrilenkovi@bk.ru>
2021-10-21 23:48:25 +0300

    Добавьте файлы проекта.

---
+++ dz6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести с клавиатуры предложение");
+            string startString = Console.ReadLine();//вводим строку
+            string[] stringArray = startString.Split();//разбиваем в массив отдельных слов
+            string a = "";//нулевая строка
+            int b = 0;//нулевое значение
+            foreach (string s in stringArray)// для каждого элемента строки в массиве выполнить действие(переменная s принимает значение поочереди
+            {
+                if (s.Length > b)//если длинна слова больше b,то b принимает значение слова 
+                {
+                    b = s.Length;//b принимает значение длины наибольшего слова 
+                    a = s;// строка принимает значение большего слова
+                }
+            }
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ z2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести с клавиатуры предложение");
+            string startString = Console.ReadLine();//вводим строку
+            string a = "";//нулевая строка
+            startString = startString.Replace(" ",a);
+            string s1 = "";
+            
+            for (int i = startString.Length-1; i >=0; i--)
+            {
+                s1+=startString[i];
+                
+            }
+            Console.WriteLine(s1);
+            if (startString==s1)
+                Console.WriteLine("Это палиндром");
+            else
+            {
+                Console.WriteLine("Это не палиндром");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
