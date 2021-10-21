2c7041697dc1c7956fee83fc7369cbc1975fe871
gavrilenkovi@bk.ru <gavrilenkovi@bk.ru>
2021-10-21 23:58:06 +0300

    1

--- z1/Program.cs
+++ z1/Program.cs
@@ -10,7 +10,21 @@ namespace z1
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Ввести с клавиатуры предложение");
+            string startString = Console.ReadLine();//вводим строку
+            string[] stringArray = startString.Split();//разбиваем в массив отдельных слов
+            string a = "";//нулевая строка
+            int b = 0;//нулевое значение
+            foreach (string s in stringArray)// для каждого элемента строки в массиве выполнить действие(переменная s принимает значение поочереди
+            {
+                if (s.Length > b)//если длинна слова больше b,то b принимает значение слова 
+                {
+                    b = s.Length;//b принимает значение длины наибольшего слова 
+                    a = s;// строка принимает значение большего слова
+                }
+            }
+            Console.WriteLine(a);
+            Console.ReadKey();
         }
     }
 }
